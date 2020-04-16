43f869970d139b095979fbdee5e58efeda1da983
mikareich <mikareich0@gmail.com>
2020-04-16 15:07:48 +0200

    backup

--- test/components/buttons/button.js
+++ test/components/buttons/button.js
@@ -1,18 +1,37 @@
 class BeautifyButton extends HTMLElement {
-  static get observedAttribute() {
+  static get observedAttributes() {
     return ['label', 'prepend-icon', 'append-icon']
   }
-  constructor() {
-    super()
-
-    this._shadowRoot = this.attachShadow({ mode: 'open' })
+  _updateStyle() {
+    this.innerHTML = `
+      <span class="prepend">${this.prependIcon || ''}</span>
+      <span class="body">${this.label}</span>
+      <span class="append">${this.appendIcon || ''}</span>`
   }
   get label() {
     return this.getAttribute('label')
   }
   set label(label) {
     this.setAttribute('label', label)
-    this._shadowRoot.innerHTML = label || ''
+  }
+  get prependIcon() {
+    return this.getAttribute('prepend-icon')
+  }
+  set prependIcon(icon) {
+    this.setAttribute('prepend-icon', icon)
+  }
+  get appendIcon() {
+    return this.getAttribute('append-icon')
+  }
+  set appendIcon(icon) {
+    this.setAttribute('append-icon', icon)
+  }
+  attributeChangedCallback() {
+    // update style
+    this._updateStyle()
+  }
+  constructor() {
+    super()
   }
 }
 
